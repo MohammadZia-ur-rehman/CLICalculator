a19891b320da89304f4eebd02a444e374e50e913
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-04-26 11:45:11 +0500

    Added Welcome Message using Figlet and Gradient

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("GIT Initialized");
-export {};
+#!/usr/bin/env node
+import figlet from "figlet";
+import gradient from "gradient-string";
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+async function welcome() {
+    console.clear();
+    figlet(`CLI Calculator`, (err, data) => {
+        console.log(
+            ` ${gradient.pastel.multiline(data)} Author: M. Zia Ur Rehman!`
+        );
+    });
+}
+await welcome();
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("GIT Initialized");
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+import gradient from "gradient-string";
+const sleep = (ms: number = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+    console.clear();
+    figlet(`CLI Calculator`, (err, data) => {
+        console.log(
+            ` ${gradient.pastel.multiline(data)} (Author: M. Zia Ur Rehman)`
+        );
+    });
+}
+
+await welcome();
